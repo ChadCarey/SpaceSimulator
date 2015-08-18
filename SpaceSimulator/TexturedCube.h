99fe20150bc11d856ee3cc851b0d7ffe40b41e77
Chad <chadcarey7@gmail.com>
2015-08-18 14:47:52 -0600

    added TexturedCube

---
+++ SpaceSimulator/TexturedCube.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Model.h"
+#include<stdarg.h>
+#include "VertexFormat.h"
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+
+/**
+* draws a cube in the most inconvenient way possible: by creating the vertex array, triangle by triangle and passing it to the shader.
+*/
+namespace Rendering
+{
+	namespace Models
+	{
+		class CubeTexture : public Model
+		{
+		public:
+			CubeTexture(float scale);
+			~CubeTexture();
+
+			void Create(float scale = 1);
+			virtual void Draw(const glm::mat4& projection_matrix, const glm::mat4& view_matrix) override final;
+		};
+	}
+}
